aff8a046a2eea1ea2396a26269f74682e8fd9edc
DEVils <techanalogy.mait@gmail.com>
2024-01-22 18:08:23 +0530

    updated model

--- website/src/components/AboutUs.jsx
+++ website/src/components/AboutUs.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function AboutUs() {
     return (
         <>
-            <section className='bg-gray-800'>
+            <section id='whatwedo' className='bg-gray-800'>
                 <div className='text-6xl p-20'>
                     What We Do
                 </div>
@@ -11,13 +11,13 @@ function AboutUs() {
 
                     <div class="max-w-lg bg-gray-700 rounded-lg shadow-lg m-10 duration-100 hover:scale-110">
                         <a href="/">
-                            <img class="rounded-t-lg" src="/bg2.webp" alt="" />
+                            <img class="rounded-t-lg" src="/1.webp" alt="" />
                         </a>
                         <div class="p-5">
                             <a href="#">
-                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Shaping Tomorrow's Virtual Ecosystems</h5>
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Emissions Monitoring and Budget Estimation</h5>
                             </a>
-                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center py-5">Leading in sustainable metaverse architecture, we fuse innovation and eco-design to craft immersive digital spaces. Join us in building a harmonious and sustainable virtual future.</p>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center py-5">Track carbon emissions in real-time, providing actionable insights for sustainable decisions. Our budget estimation tools help optimize resource use, ensuring eco-friendly practices align with financial goals.</p>
                             <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 View in Metaverse
                                 <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
@@ -28,13 +28,13 @@ function AboutUs() {
                     </div>
                     <div class="max-w-lg bg-gray-700 rounded-lg shadow-lg m-10 duration-100 hover:scale-110">
                         <a href="/">
-                            <img class="rounded-t-lg" src="/bg2.webp" alt="" />
+                            <img class="rounded-t-lg" src="/2.jpg" alt="" />
                         </a>
                         <div class="p-5">
                             <a href="#">
-                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Architects of Digital Sustainability</h5>
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Customization & Immersive Integration</h5>
                             </a>
-                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center py-5">At the forefront of metaverse design, we seamlessly integrate innovation and eco-responsibility. Explore our commitment to crafting immersive, sustainable digital environments for a better tomorrow.</p>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center py-5">Tailor experiences to user preferences through immersive integration. From personalized avatars to dynamic environments, we create engaging, unique metaverse encounters that captivate and resonate with each user.</p>
                             <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 View in Metaverse
                                 <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
@@ -45,13 +45,13 @@ function AboutUs() {
                     </div>
                     <div class="max-w-lg bg-gray-700 rounded-lg shadow-lg m-10 duration-100 hover:scale-110">
                         <a href="/">
-                            <img class="rounded-t-lg" src="/bg2.webp" alt="" />
+                            <img class="rounded-t-lg" src="/3.png" alt="" />
                         </a>
                         <div class="p-5">
                             <a href="#">
-                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Crafting Sustainable Virtual Realities</h5>
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Multiplayer Connectivity Platform</h5>
                             </a>
-                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center py-5">As pioneers in metaverse architecture, we marry innovation and eco-design. Embark on a journey with us to create immersive, sustainable digital realms that redefine the future of virtual experiences.</p>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center py-5">Forge connections in our metaverse with a robust multiplayer platform. Collaborate seamlessly, engaging users in shared experiences that transcend geographical boundaries. Our connectivity solutions redefine interactive possibilities in the virtual realm.</p>
                             <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 View in Metaverse
                                 <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
@@ -62,13 +62,30 @@ function AboutUs() {
                     </div>
                     <div class="max-w-lg bg-gray-700 rounded-lg shadow-lg m-10 duration-100 hover:scale-110">
                         <a href="/">
-                            <img class="rounded-t-lg" src="/bg2.webp" alt="" />
+                            <img class="rounded-t-lg" src="/4.jpg" alt="" />
                         </a>
                         <div class="p-5">
                             <a href="#">
-                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Architects of Ethereal Harmony</h5>
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Transition to Sustainable Alternatives</h5>
                             </a>
-                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center py-5">Embark on a journey with us, where pixels meet purpose. As architects of ethereal harmony, we blend sustainability with metaverse magic, crafting immersive digital worlds that transcend imagination.</p>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center py-5">Navigate the metaverse sustainably. We guide your transition to eco-friendly practices and alternatives, ensuring a seamless shift towards a virtual environment that aligns with environmental and social responsibility.</p>
+                            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                View in Metaverse
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="max-w-lg bg-gray-700 rounded-lg shadow-lg m-10 duration-100 hover:scale-110">
+                        <a href="/">
+                            <img class="rounded-t-lg" src="/5.jpg" alt="" />
+                        </a>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Cost Benefit Analysis</h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center py-5">Uncover the financial advantages of sustainability with our cost-benefit analysis. We evaluate the economic impact of eco-conscious choices, providing a clear roadmap for decisions that harmonize financial and environmental considerations.</p>
                             <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 View in Metaverse
                                 <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
--- website/src/views/Home.jsx
+++ website/src/views/Home.jsx
@@ -12,7 +12,7 @@ export default function componentName() {
 					<h1 class="mb-4 text-4xl font-bold tracking-tight leading-none text-gray-800">Sustainabilty in Architecture using <br /><span className="text-7xl bg-gradient-to-r from-blue-500 via-green-600 to-blue-500 inline-block text-transparent bg-clip-text">Metaverse</span></h1>
 					<p class="my-16 text-white font-semibold text-xl px-20">Designing eco-friendly virtual spaces for a harmonious future. Explore, inspire, and transform with us by crafting digital spaces that have eco-conscious design. Experience the future of virtual architecture now!</p>
 					<div class="flex flex-row-2 space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-						<a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center border-4 border-blue-700 text-blue-700 rounded-lg bg-white hover:bg-blue-800 hover:text-white focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 w-64 mx-10 h-12">
+						<a href="#whatwedo" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center border-4 border-blue-700 text-blue-700 rounded-lg bg-white hover:bg-blue-800 hover:text-white focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 w-64 mx-10 h-12">
 							Services
 						</a>
 						<a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 w-64 mx-10  h-12">
